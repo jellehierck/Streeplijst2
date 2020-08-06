398f9a42d948111f5f02571799b7c87c586935b8
jhierck laptop <60922735+jhierck@users.noreply.github.com>
2020-08-06 14:49:06 +0200

    Cleaned up code and added docstrings to all previous work

--- streeplijst/api.py
+++ streeplijst/api.py
@@ -3,27 +3,17 @@ import json
 
 from streeplijst.config import BASE_URL, base_header
 
+
 def get_products_in_folder(folder_id):
     """
-    GET all products inside a single folder from Congressus API.
+    GET all products inside a single folder from Congressus API. This is a blocking call.
 
     :param folder_id: Folder id to retrieve items for.
     :return: A list of dicts containing the server response converted from a JSON string.
     """
-    url = BASE_URL + "products?folder_id=" + str(folder_id)
-    res = requests.get(url=url, headers=base_header())
-    res.json()
-    return_list = json.loads(res.text)
+    url = BASE_URL + "products?folder_id=" + str(folder_id)  # Set the URL to connect to the API
+    headers = base_header()  # Create the base header which contains the API token
+    res = requests.get(url=url, headers=headers)  # Send the request
+    res.json()  # Convert response to JSON
+    return_list = json.loads(res.text)  # Select the relevant data and convert to a list of dicts
     return return_list
-
-
-def get_all_products():
-    """
-    GET all products in the folders specified in config.py.
-
-    :return: all products.
-    """
-    pass
-
-
-get_products_in_folder(1998)
--- streeplijst/items.py
+++ streeplijst/items.py
@@ -3,6 +3,7 @@ import datetime
 from streeplijst.config import FOLDERS
 import streeplijst.api as api
 
+
 def get_folder_from_config(folder_name):
     """
     Reads the config.py file and returns the Folder object with that name.
@@ -10,38 +11,69 @@ def get_folder_from_config(folder_name):
     :param folder_name: The folder name to read.
     :return: The Folder object
     """
-    folder_dict = FOLDERS[folder_name]
-    folder = Folder(folder_dict["name"], folder_dict["id"], folder_dict["media"])
+    folder_dict = FOLDERS[folder_name]  # Load the folder configuration
+    folder = Folder(folder_dict["name"], folder_dict["id"], folder_dict["media"])  # Create Folder object
     return folder
 
 
 class Folder:
     def __init__(self, name, id, media=""):
-        self.name = name  # Folder name
-        self.id = id  # Folder id
-        self.media = media  # Optional image URL
-        self.last_updated = datetime.datetime.now().isoformat()
-        self.items = self.get_items()  # Dict for all items
+        """
+        Instantiates a Folder object.
+
+        :param name: Folder name
+        :param id: Folder id
+        :param media: (optional) Image URL
+        """
+        self.name = name
+        self.id = id
+        self.media = media
+        self.last_updated = datetime.datetime.now().isoformat()  # Set the last updated time to now
+        self.items = self.get_items()
 
     def get_items(self):
-        items_list = api.get_products_in_folder(self.id)
-        result = dict()
-        for item_dict in items_list:
+        """
+        GET all items from API.
+
+        :return: A dict with (key: value) (item id: GetItem object)
+        """
+        items_list = api.get_products_in_folder(self.id)  # Make the API call to get items in the folder
+        result = dict()  # Empty dict to store items in
+        for item_dict in items_list:  # Iterate all items in the response
             result[item_dict["id"]] = GetItem(item_dict["name"], item_dict["id"], item_dict["price"],
-                                                  item_dict["folder"], item_dict["folder_id"], item_dict["published"],
-                                                  item_dict["media"])
+                                              item_dict["folder"], item_dict["folder_id"], item_dict["published"],
+                                              item_dict["media"])  # Create GetItem object
+        self.last_updated = datetime.datetime.now().isoformat()  # Set the last updated time to now
         return result
 
 
 class BaseItem:
     def __init__(self, name, id, price):
+        """
+        Instantiate a BaseItem object. This Item contains the very basic information of items.
+
+        :param name: Item name
+        :param id: Item id
+        :param price: Item price
+        """
         self.name = name
         self.id = id
         self.price = price
 
 
 class GetItem(BaseItem):
-    def __init__(self, name, id, price, folder, folder_id, published, media):
+    def __init__(self, name, id, price, folder, folder_id, published, media=""):
+        """
+        Instantiate a GetItem object. This Item contains all relevant information provided by the API response.
+
+        :param name: Item name
+        :param id: Item id
+        :param price: Item price
+        :param folder: Folder
+        :param folder_id: Folder id
+        :param published: True if the item is published, false otherwise
+        :param media: (optional) Image URL
+        """
         super().__init__(name, id, price)
         self.folder = folder
         self.folder_id = folder_id
@@ -51,5 +83,12 @@ class GetItem(BaseItem):
 
 class PostItem(BaseItem):
     def __init__(self, name, id, price, quantity):
+        """
+        Instantiate a PostItem object. This Item contains all relevant information to POST a sale to the API
+        :param name: Item name
+        :param id: Item id
+        :param price: Item price
+        :param quantity: Amount of this item to buy
+        """
         super().__init__(name, id, price)
         self.quantity = quantity
