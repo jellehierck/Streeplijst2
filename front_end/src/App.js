0159acfe2a9227953cef6d154f7551800a8aaf03
jhierck laptop <60922735+jhierck@users.noreply.github.com>
2020-08-07 11:14:30 +0200

    Cleanup

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,8 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const [currentTime, setCurrentTime] = useState(0)
+    // Based on: https://blog.miguelgrinberg.com/post/how-to-create-a-react--flask-project
 
+    const [currentTime, setCurrentTime] = useState(0)
     useEffect(() => {
         fetch('/time')
         .then(res => res.json())
