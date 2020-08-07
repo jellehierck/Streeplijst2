188264803d1f50ed9bf5ba0bbfd3498527094827
jhierck laptop <60922735+jhierck@users.noreply.github.com>
2020-08-07 22:56:00 +0200

    Added api calls to user login screen.
    Added SECRET_KEY and DEV_KEY to credentials.py (credentials.py is not added to the repository).
    Added login logic to app.py.
    Fixed login.html to have an input field to actually submit a form with data.
    Added default flashed messages section to main_grid.html in the bottom grid container.

--- streeplijst2/app.py
+++ streeplijst2/app.py
@@ -4,18 +4,37 @@ app.py
 Contains all Flask logic to connect this Python backend to the HTML frontend.
 """
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash
 
 import streeplijst2.streeplijst as streeplijst
 import streeplijst2.api as api
+from credentials import DEV_KEY, SECRET_KEY
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config.from_mapping(
+            SECRET_KEY=DEV_KEY,
+    )
 
+    # Landing page
     @app.route('/')
     def index():
-        return render_template("login.html")
+        return redirect(url_for('login'))
+
+    # Login page. When called with GET, this loads the login screen. When called with POST, attempts to login user.
+    @app.route('/login', methods=('GET', 'POST'))
+    def login():
+        if request.method == 'GET':  # Load the login page to let users enter their s-number
+            return render_template('login.html')
+
+        elif request.method == 'POST':  # Attempt to login the user
+            s_number = request.form['student-number']
+            user = streeplijst.User(s_number)
+            flash(user.first_name)
+            return redirect(url_for('login'))
+
+
 
     @app.route('/time')
     def get_current_time():
