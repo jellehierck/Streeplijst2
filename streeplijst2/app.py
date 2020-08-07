d55e7df6d9c6e3e769610a0ba69ac06972b86110
jhierck laptop <60922735+jhierck@users.noreply.github.com>
2020-08-07 22:19:30 +0200

    Renamed login.html to login_page.html. Changed references in app.py

--- streeplijst2/app.py
+++ streeplijst2/app.py
@@ -4,7 +4,7 @@ app.py
 Contains all Flask logic to connect this Python backend to the HTML frontend.
 """
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 import streeplijst2.streeplijst as streeplijst
 import streeplijst2.api as api
@@ -13,13 +13,15 @@ import streeplijst2.api as api
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # Initial page loaded on startup
     @app.route('/')
     def index():
-        return render_template("login.html")
+        return redirect(url_for('login_page'))  # Redirects to the login page by default
 
-    @app.route('/time')
-    def get_current_time():
-        return {'time': datetime.datetime.now().isoformat()}
+    # Login page
+    @app.route('/login_page')
+    def login():
+        return render_template("login_page.html")  # Render the Jinja HTML template for login
 
     return app
 
