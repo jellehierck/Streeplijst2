cd0c840aea74f9d18978f0383b36dd2a8a2bd4bf
jhierck laptop <60922735+jhierck@users.noreply.github.com>
2020-11-21 17:40:47 +0100

    Small bugfix

--- streeplijst2/main.py
+++ streeplijst2/main.py
@@ -4,28 +4,31 @@ from streeplijst2 import create_app
 from streeplijst2.config import PORT
 
 if __name__ == '__main__':
+    # Use the argparse library to handle support for command line options when running the app from a console
     parser = ArgumentParser(prog='Streeplijst2',
                             description='Paradoks streeplijst application, created by Jelle Hierck.')
 
+    # Hostname argument (e.g. localhost, 127.0.0.1, 123.456.78.90)
     parser.add_argument('host',
                         nargs='?',
                         default='localhost',
                         help='hostname for this app. Default: localhost')
 
+    # Port nr argument (e.g. 5000)
     parser.add_argument('-p',
                         '--port',
                         type=int,
                         default=PORT,
                         help='port number for this app')
 
+    # Run in debug mode argument
     parser.add_argument('-d',
                         '--debug',
                         action='store_true',
                         # Defaults to False
                         help='run this app in debug mode')
 
-    args = parser.parse_args()
+    args = parser.parse_args()  # Parse the arguments
 
-    app = create_app()
+    app = create_app()  # Create flask app
     app.run(host=args.host, port=args.port, debug=args.debug)
-    app.run()
