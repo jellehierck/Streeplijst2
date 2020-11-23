8ce2f8e5026a0758aa849eeea2fecce3012baac5
jhierck laptop <60922735+jhierck@users.noreply.github.com>
2020-11-23 17:00:48 +0100

    Removed caching (deprecated)

--- streeplijst2/extensions.py
+++ streeplijst2/extensions.py
@@ -1,11 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_caching import Cache
-
-# Caching for temporarily storing results of time-consuming requests (e.g. get folders)
-cache = Cache(config={
-    'CACHE_TYPE': 'simple',
-    'CACHE_DEFAULT_TIMEOUT': 300
-})
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/database.sqlite'
 
